09be2f13410e3aae901708c9f0f4ece091ae4cee
Thepharaoh49 <Thepharaoh49@hotmail.co.uk>
2025-05-14 09:16:10 +0100

    Added readobject model for Floor

--- source/DC2AP/Models/Floor.cs
+++ source/DC2AP/Models/Floor.cs
@@ -20,4 +20,49 @@ namespace DC2AP.Models
         public int MonstersKilled { get; set; }
         public int TimesVisited { get; set; }
     }
+	/*
+	public class Floor
+	{
+		// First byte with bit flags
+		[MemoryOffset(0x00, bitPosition: 0)]
+		public bool Unlocked { get; set; }
+		
+		[MemoryOffset(0x00, bitPosition: 1)]
+		public bool Completed { get; set; }
+		
+		[MemoryOffset(0x00, bitPosition: 3)]
+		public bool SpecialMedalCompleted { get; set; } // Special
+		
+		[MemoryOffset(0x00, bitPosition: 4)]
+		public bool ClearMedalCompleted { get; set; } // Clear
+		
+		[MemoryOffset(0x00, bitPosition: 5)]
+		public bool FishMedalCompleted { get; set; } // Fish
+		
+		[MemoryOffset(0x00, bitPosition: 7)]
+		public bool SphedaMedalCompleted { get; set; } // Spheda
+		
+		// Second byte with bit flags (optional bits 0 and 1)
+		[MemoryOffset(0x01, bitPosition: 0)]
+		public bool GotGeostone { get; set; }
+		
+		[MemoryOffset(0x01, bitPosition: 1)]
+		public bool DownloadedGeostone { get; set; }
+		
+		[MemoryOffset(0x01, bitPosition: 2)]
+		public bool KilledAllMonsters { get; set; }
+		
+		// Next 2 bytes: ushort for monsters killed
+		[MemoryOffset(0x02)]
+		public ushort MonstersKilled { get; set; }
+		
+		// Next 2 bytes: ushort for times visited
+		[MemoryOffset(0x04)]
+		public ushort TimesVisited { get; set; }
+		
+		// Unknown data section (14 bytes)
+		[MemoryOffset(0x06, collectionLength: 14)]
+		public List<byte> UnknownData { get; set; }
+	}
+	*/
 }
