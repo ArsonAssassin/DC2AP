b320dc2cf16178268c00f407d8d7aa59e0888a88
Thepharaoh49 <Thepharaoh49@hotmail.co.uk>
2025-12-18 11:06:00 +0000

    add missing files

---
+++ source/DC2AP/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DC2AP
+{
+    public class Constants
+    {
+        public const int AP_ID_OFFSET = 694200000;
+        public const int MAX_INVENTORY_SLOTS = 144;
+    }
+}
---
+++ source/DC2AP/Models/ItemId.cs
@@ -0,0 +1,14 @@
+﻿using Archipelago.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DC2AP.Models
+{
+    public class ItemId : Item
+    {
+        public long LocationId { get; set; }
+    }
+}
